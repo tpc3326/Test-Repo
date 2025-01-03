055c3baeb7c1dccbd66da6c474eb01e4408e076c
tpc3326 <nigel.fernandes@landg.com>
2025-01-03 09:43:03 +0000

    Modification 2

--- src/nig.java
+++ src/nig.java
@@ -3,5 +3,6 @@ public class nig {
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
         System.out.println("Modification 1");
+        System.out.println("Modification 2");
     }
 }
